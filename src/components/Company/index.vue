eb926ec1436e513062e6c407af79c82642ab131e
2782 <lhhoa@bbs.co.jp>
2019-08-16 17:35:17 +0900

    nothing

--- src/components/Company/index.vue
+++ src/components/Company/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout justify-center class="company-home-page">
+  <v-layout row justify-center class="company-home-page">
     <v-flex md12>
       <v-card flat class="mx-auto">
         <v-list-item>
@@ -64,21 +64,34 @@
             </v-col>
           </v-row>
         </v-container>
-
-        <v-card-text>Visit ten places on our planet that are undergoing the biggest changes today.</v-card-text>
-
-        <!-- <v-card-actions>
-          <v-btn text color="deep-purple accent-4">Read</v-btn>
-          <v-btn text color="deep-purple accent-4">Bookmark</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
-          <v-btn icon>
-            <v-icon>mdi-share-variant</v-icon>
-          </v-btn>
-        </v-card-actions>-->
       </v-card>
+    </v-flex>
+
+    <v-flex md12>
+      <v-container fluid>
+        <v-layout justify-center>
+          <v-flex md8 d-flex>
+            <p>
+              D:\mypj\turbo-octo-happiness>npm install vuetify
+              npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@1.2.9 (node_modules\fsevents):
+              npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@1.2.9: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+              + vuetify@2.0.7
+              updated 1 package in 26.286s
+              [+] no known vulnerabilities found [30891 packages audited]
+            </p>
+          </v-flex>
+          <v-flex md4 d-flex>
+            <p>
+              D:\mypj\turbo-octo-happiness>npm install vuetify
+              npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@1.2.9 (node_modules\fsevents):
+              npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@1.2.9: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+              + vuetify@2.0.7
+              updated 1 package in 26.286s
+              [+] no known vulnerabilities found [30891 packages audited]
+            </p>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
